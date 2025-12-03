01bdb1c5ac87b4e041a3db4cab644e4ff16c230e
Devin Abbott <devinabbott@gmail.com>
2025-12-03 13:22:05 -0800

    fix packaging

--- src/ifs.ts
+++ src/ifs.ts
@@ -1,10 +1,37 @@
-import type { Stats } from 'fs'
+export interface StatsLike {
+  dev: number
+  ino: number
+  mode: number
+  nlink: number
+  uid: number
+  gid: number
+  rdev: number
+  size: number
+  blksize: number
+  blocks: number
+  atimeMs: number
+  mtimeMs: number
+  ctimeMs: number
+  birthtimeMs: number
+  atime: Date
+  mtime: Date
+  ctime: Date
+  birthtime: Date
+  isFile(): boolean
+  isDirectory(): boolean
+  isBlockDevice(): boolean
+  isCharacterDevice(): boolean
+  isSymbolicLink(): boolean
+  isFIFO(): boolean
+  isSocket(): boolean
+  toJSON?(): unknown
+}
 
 // Minimal fs-like interface used by this library
 // Compatible with Node's fs and memfs implementations
 export interface IFS {
-  lstatSync(path: string): Stats
-  statSync(path: string): Stats
+  lstatSync(path: string): StatsLike
+  statSync(path: string): StatsLike
   readdirSync(path: string): string[]
   readFileSync(path: string): Buffer
   readFileSync(path: string, encoding: 'utf8'): string
