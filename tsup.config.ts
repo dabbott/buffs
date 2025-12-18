773ef12305cb1ed61f58bbae5b2b2493f9d00402
Devin Abbott <devinabbott@gmail.com>
2025-12-17 18:26:09 -0800

    export

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/memory-fs.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   clean: true,
@@ -14,4 +14,3 @@ export default defineConfig({
     }
   },
 })
-
